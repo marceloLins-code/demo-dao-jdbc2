93e5cf6b05ec4d97ee36f44df3126c207c9dbadb
Marcelo Medeiros <marcelolinsdemedeiros@gmail.com>
2020-09-20 11:15:06 -0300

    SellerDaoJDBC and DaoFactory*

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,7 +1,13 @@
 package model.dao;
 
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
 	
-	
+	public static SellerDao createSellerdao() {
+		return new SellerDaoJDBC();
+		
+		
+	}
 
 }

5efaf13344ce8927a1ccc2e1b8f75d0c47b4c617
Marcelo Medeiros <marcelolinsdemedeiros@gmail.com>
2020-09-20 10:42:53 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
@@ -28,6 +30,44 @@ public SellerDaoJDBC() {}
 
 	@Override
 	public void insert(Seller obj) {
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.clientPrepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthdate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDep().getId());
+			
+			int roesAffected = st.executeUpdate();
+			
+			if (roesAffected >0) {
+				ResultSet rs = st.getGeneratedKeys();
+						if (rs.next()) {
+							int id = rs.getInt(1);
+							obj.setId(id );
+							
+						}
+						DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("unexpected error! no rows affected" );
+
+			}
+			
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+				}
 		
 		
 	}
